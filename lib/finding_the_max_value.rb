6783fb3650aa1a5a534d64c25031d4efeb26f43d
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 16:06:42 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ require 'pry'
 def find_max_value(array)
   # Add your solution here
   high_number = array[0]
-  
+  binding.pry
   for i in 1...(array.length - 1)
     if array[i] > high_number
       high_number = array[i]
